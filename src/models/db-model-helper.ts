88ee46623dfe61374df74494256b1dbf7f443825
Ashish Shakya <shakyashish4@gmail.com>
2022-07-05 16:16:37 +0530

    feat: Restructure db model helper

--- src/models/db-model-helper.ts
+++ src/models/db-model-helper.ts
@@ -1,3 +1,4 @@
+import { Sequelize } from 'sequelize';
 import DbDriver from '../db-driver/db-driver';
 import Logger from '../logger/logger';
 
@@ -6,12 +7,17 @@ import buildUserModel from './user.model';
 class DbModelHelper {
   private logger: Logger;
   private dbDriver: DbDriver;
-  private dbModelBuilders = [buildUserModel];
+  private dbModelBuilders: ((sequelize: Sequelize) => void)[];
 
   constructor({ logger, dbDriver }) {
     this.logger = logger;
     this.dbDriver = dbDriver;
+    this.populateDbModelBuilders();
   }
+
+  private populateDbModelBuilders = () => {
+    this.dbModelBuilders = [buildUserModel];
+  };
 
   initializeDBModels = async () => {
     const sequelize = await this.dbDriver.getDBConnection();
